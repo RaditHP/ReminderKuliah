f59ff78d7f55805149090284a8c3bf9c59ec6615
RHP <rpramudito@gmail.com>
2018-05-06 16:53:19 +0700

    Projext PAPB Lanjut

---
+++ app/src/main/java/com/example/rhp/remiderkuliah/ProfileActivity.java
@@ -0,0 +1,12 @@
+package com.example.rhp.remiderkuliah;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_reminder);
+    }
+}
